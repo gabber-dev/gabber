ad8af71fb3cc882ecd4b47719c9390c3157a4169
Jack <jackndwyer@gmail.com>
2025-07-30 15:50:03 -0700

    favicon and logo and readme header image (#1)
    * favicon and logo
    
    * readme banner
    
    ---------
    
    Signed-off-by: Jack <jackndwyer@gmail.com>
    Co-authored-by: Jack Dwyer <jackdwyer@macbookair.local.meter>

--- frontend/components/TopBar.tsx
+++ frontend/components/TopBar.tsx
@@ -6,14 +6,22 @@
 "use client";
 // import { useParams } from "next/navigation";
 import Link from "next/link";
+import Image from "next/image";
+import { ClipboardDocumentIcon, CheckIcon } from "@heroicons/react/24/outline";
 
 export function TopBar() {
   // const params = useParams<{ app?: string; graph?: string }>();
 
   return (
     <div className="flex items-center gap-2 bg-base-200 h-full w-full px-2">
-      <Link href={`/`} className="">
-        Gabber
+      <Link href={`/`} className="flex items-center py-2">
+        <Image
+          src="/logo.png"
+          alt="Gabber"
+          width={100}
+          height={100}
+          className="object-contain"
+        />
       </Link>
       <Link href={`/example`}>Examples</Link>
     </div>
