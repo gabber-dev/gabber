7c5cda5e5b0b3f77b222e29e8a39ae9dfba7dade
Keepingitneil <neildwyer1991@gmail.com>
2025-08-04 10:10:03 -0700

    react sdk work

--- sdks/react/src/useSourcePad.tsx
+++ sdks/react/src/useSourcePad.tsx
@@ -1,27 +1,21 @@
 import { PadTriggeredValue, SourcePad } from "@gabber/client";
 import { useEngineInternal } from "./useEngine";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { usePad } from "./usePad";
 
-type UseSourcePadType = {
-    lastValue: PadTriggeredValue | undefined;
+type UseSourcePadType<DataType extends PadTriggeredValue> = {
+    pushValue: (v: DataType) => Promise<void>;
 }
 
-export function useSourcePad<DataType extends PadTriggeredValue>(nodeId: string, padId: string): UseSourcePadType {
-    const [lastValue, setLastValue] = useState<DataType | undefined>(undefined);
+export function useSourcePad<DataType extends PadTriggeredValue>(nodeId: string, padId: string): UseSourcePadType<DataType> {
     const { engineRef } = useEngineInternal();
     const padRef = useRef<SourcePad<DataType>>(engineRef.current.getSourcePad<DataType>(nodeId, padId));
-    const padHandlerRef = useRef((value: DataType) => {
-        setLastValue(value);
-    });
 
-    useEffect(() => {
-        padRef.current.on('value', padHandlerRef.current);
-        return () => {
-            padRef.current.off('value', padHandlerRef.current);
-        };
-    }, [padRef]);
+    const pushValue = useCallback(async (v: DataType) => {
+        await padRef.current.pushValue(v);
+    }, [engineRef, nodeId, padId]);
 
     return {
-        lastValue,
+        pushValue,
     }
 }
