333ba54c3b6527c962b0e10b80cdecedd63bb643
Keepingitneil <neildwyer1991@gmail.com>
2025-08-07 09:25:19 -0700

    add qwen service + fix deserialization

--- engine/src/nodes/core/tool/tool.py
+++ engine/src/nodes/core/tool/tool.py
@@ -69,6 +69,7 @@ class Tool(node.Node):
         schema = cast(runtime_types.Schema, schema_pad.get_value())
         if not schema:
             schema = runtime_types.Schema(properties={})
+        logging.info(f"NEIL Tool schema: {schema}")
         source = cast(pad.StatelessSourcePad, self.get_pad("source"))
         if not source:
             source = pad.StatelessSourcePad(
