23bdd3288f5cd177c08c3bdd1a849d8a7f29c491
Neil Dwyer <neildwyer1991@gmail.com>
2025-08-14 16:11:31 -0700

    Introduce ContextMessageZip to create + synchronize context messages with multiple content items (#57)
    * Delay node
    
    * working on context builder
    
    * Ability to synchronize context message building
    
    * remove confusing waitforrequest node

--- engine/src/nodes/core/utility/__init__.py
+++ engine/src/nodes/core/utility/__init__.py
@@ -7,12 +7,10 @@ from .enum_switch import EnumSwitch
 from .merge import Merge
 from .type_constraint import TypeConstraint
 from .unpack_object import UnpackObject
-from .wait_for_request import WaitForRequest
 from .noop import Noop
 
 ALL_NODES = [
     Merge,
-    WaitForRequest,
     Entry,
     TypeConstraint,
     AutoConvert,
