c2483cfecde1ad00354c1e12297682aeeacd6d8b
Keepingitneil <neildwyer1991@gmail.com>
2025-08-18 17:00:38 -0700

    docker compose + provide examples

--- services/local-llm/qwen-omni-7b-q4km_llamacpp.sh
+++ services/local-llm/qwen-omni-7b-q4km_llamacpp.sh
@@ -7,7 +7,7 @@ LLAMA_SERVER_PATH=${LLAMA_SERVER_PATH:-llama-server}
   --hf-file Qwen2.5-Omni-7B-Q4_K_M.gguf \
   --mmproj-url 'https://huggingface.co/unsloth/Qwen2.5-Omni-7B-GGUF/resolve/main/mmproj-F16.gguf' \
   -cb \
-  -np 4 \
+  -np 2 \
   -c 8192 \
   --port 7002 \
   --host 0.0.0.0
