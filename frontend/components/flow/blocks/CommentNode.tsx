cd52a3430c0bc6885b846f895f928db354660c53
Jack <jackndwyer@gmail.com>
2025-08-21 15:55:17 -0700

    Styling (#83)
    * comment node adjusted
    
    * click to copy block id

--- frontend/components/flow/blocks/CommentNode.tsx
+++ frontend/components/flow/blocks/CommentNode.tsx
@@ -4,7 +4,6 @@
  */
 
 import { NodeEditorRepresentation } from "@/generated/editor";
-import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/outline";
 import { MultiLineTextPropertyEdit } from "./components/pads/property_edit/MultiLineTextPropertyEdit";
 
 export interface CommentNodeProps {
@@ -12,24 +11,11 @@ export interface CommentNodeProps {
 }
 
 export function CommentNode({ data }: CommentNodeProps) {
-
-
   return (
-    <div className="min-w-80 w-full flex flex-col bg-base-200 border-2 border-accent border-b-4 border-r-4 rounded-lg relative">
-      <div className="flex w-full items-center gap-2 bg-base-300 border-b-2 border-accent p-3 rounded-t-lg drag-handle cursor-grab active:cursor-grabbing">
-        <ChatBubbleLeftRightIcon className="h-5 w-5 text-accent" />
-        <div className="flex-1">
-          <h2 className="text-lg text-primary font-medium">
-            {data.editor_name}
-          </h2>
-          <div className="text-xs text-base-content/60 font-mono">
-            {data.id}
-          </div>
-        </div>
-      </div>
-
+    <div className="min-w-80 w-full flex flex-col bg-base-100 rounded-lg relative">
+      <div className="h-2 bg-warning/20 rounded-t-lg drag-handle cursor-grab active:cursor-grabbing"></div>
       <div className="flex flex-col p-4">
-        <div className="[&_.pad-handle]:hidden [&_.pad-label]:hidden [&_textarea]:min-h-[72px]">
+        <div className="[&_.pad-handle]:hidden [&_.pad-label]:hidden [&_textarea]:min-h-[72px] [&_textarea]:bg-base-100 [&_textarea]:text-warning [&_textarea]:border-transparent [&_textarea]:placeholder:text-warning/50 [&_textarea]:focus:border-transparent">
           <MultiLineTextPropertyEdit nodeId={data.id} padId="text" />
         </div>
       </div>
