8213fcba11eded80bd0f5037aaafd12d0af92967
Neil Dwyer <neildwyer1991@gmail.com>
2025-08-21 10:16:37 -0700

    Introducing Deepgram STT (#84)
    * Working on deepgram support for STT
    
    * Introduce Deepgram STT
    
    * Deepgram w/ utterance
    
    * logging cleanup

--- engine/src/lib/stt/deepgram/__init__.py
+++ engine/src/lib/stt/deepgram/__init__.py
@@ -1,8 +1,8 @@
 # Copyright 2025 Fluently AI, Inc. DBA Gabber. All rights reserved.
 # SPDX-License-Identifier: SUL-1.0
 
-from .assembly import Assembly
+from .deepgram import Deepgram
 
 __all__ = [
-    "Assembly",
+    "Deepgram",
 ]
