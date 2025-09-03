765909afe5a17632aaa0174074d1f34004ccef02
Neil Dwyer <neildwyer1991@gmail.com>
2025-09-03 12:20:58 -0700

    Fix logs + EnumSwitchProperty node (#120)
    * Logs
    
    * bump
    
    * Introduce enum switch property

--- engine/src/nodes/core/logic/__init__.py
+++ engine/src/nodes/core/logic/__init__.py
@@ -3,6 +3,7 @@
 
 from . import state_machine
 from .enum_switch_trigger import EnumSwitchTrigger
+from .enum_switch_property import EnumSwitchProperty
 from .gate import Gate
 from .filter import Filter
 from .compare import Compare
@@ -10,4 +11,12 @@ from .and_node import And
 from .or_node import Or
 
 
-ALL_NODES = [EnumSwitchTrigger, Gate, Compare, And, Or, Filter] + state_machine.ALL_NODES
+ALL_NODES = [
+    EnumSwitchTrigger,
+    EnumSwitchProperty,
+    Gate,
+    Compare,
+    And,
+    Or,
+    Filter,
+] + state_machine.ALL_NODES
