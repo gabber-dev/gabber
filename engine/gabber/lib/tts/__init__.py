8e6c1cc81fcafcb377ed5ac49697ac62800eb564
Keepingitneil <neildwyer1991@gmail.com>
2025-09-23 17:18:16 -0700

    OpenAI TTS Support

--- engine/gabber/lib/tts/__init__.py
+++ engine/gabber/lib/tts/__init__.py
@@ -4,12 +4,14 @@
 from .cartesia_tts import CartesiaTTS
 from .elevenlabs_tts import ElevenLabsTTS
 from .gabber_tts import GabberTTS
+from .openai_tts import OpenAITTS
 from .tts import TTS, TTSSession
 
 __all__ = [
     "GabberTTS",
     "ElevenLabsTTS",
     "CartesiaTTS",
+    "OpenAITTS",
     "TTS",
     "TTSSession",
 ]
