8612751f9ddcf45518590e5b4de8685284f9c03c
Keepingitneil <neildwyer1991@gmail.com>
2025-09-25 16:27:33 -0700

    Collapsible logs

--- frontend/hooks/useRun.tsx
+++ frontend/hooks/useRun.tsx
@@ -45,7 +45,7 @@ export function RunProvider({
   generateConnectionDetailsImpl,
 }: RunProviderProps) {
   return (
-    <EngineProvider>
+    <EngineProvider maxLogItems={1000}>
       <Inner generateConnectionDetailsImpl={generateConnectionDetailsImpl}>
         {children}
       </Inner>
