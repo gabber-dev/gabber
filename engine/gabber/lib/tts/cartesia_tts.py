43b51d1ee73295ec2037a5a441b1dff23faf0c4b
Neil Dwyer <neildwyer1991@gmail.com>
2025-09-26 09:30:46 -0700

    Protect against no input (#189)

--- engine/gabber/lib/tts/cartesia_tts.py
+++ engine/gabber/lib/tts/cartesia_tts.py
@@ -39,6 +39,7 @@ class CartesiaTTS(MultiplexWebSocketTTS):
         text = self._emoji_remover.push_text(text)
         text = self._parenthesis_remover.push_text(text)
         text = self._italic_remover.push_text(text)
+
         return {
             "model_id": self._model_id,
             "max_buffer_delay_ms": 250,
